345103e38b738cbbd74d76d69d3fffbcc90eb976
heedong12 <dlgmldus3289@naver.com>
2024-06-08 00:18:22 +0900

    feat: make headerbar

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mt-8 px-2 flex flex-row w-full">
+    <img
+      src="/public/image/header-previous.png"
+      @click="handleClickMoveToStep1"
+    />
+    <div class="flex-1"></div>
+  </div>
+</template>
+
+<script>
+import router from "@/router/router";
+
+export default {
+  methods: {
+    handleClickMoveToStep1() {
+      this.$router.go(-1);
+    },
+  },
+};
+</script>
+
+<style></style>
