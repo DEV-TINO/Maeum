0749be734b1bbc347ec734c495148aa26b210236
heedong12 <dlgmldus3289@naver.com>
2024-06-08 19:17:12 +0900

    feat: make loading page

---
+++ src/components/LoadingPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="h-full flex flex-col justify-start items-center bg-result-bacground bg-no-repeat bg-bottom"
+  >
+    <h1 class="w-5/6 mt-28 mb-8 text-left font-bold text-xl text leading-9">
+      스크립트를 생성하고 있어요. <br />
+      잠시만 기다려주세요!
+    </h1>
+    <div class="h-56 bg-white w-5/6 rounded-2xl p-6 font-semibold break-keep">
+      축사는 보통 2~5분 정도의 길이를 가지는 것이 일반적이에요. 하지만 이러한
+      길이는 상황에 따라 다를 수 있어요. 행사가 길거나 청중의 집중력이 떨어질
+      경우, 축사를 짧게 유지하는 것이 좋을 수 있어요!
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
