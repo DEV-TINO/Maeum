705fdad18967a95e871dc3abc8548af6564ec45f
heedong12 <dlgmldus3289@naver.com>
2024-06-08 19:17:03 +0900

    feat: make step 6 page

---
+++ src/components/Step6Page.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="flex flex-col text-xl text-text-color px-6 h-full">
+    <div class="flex-1 flex flex-col pt-10 gap-4">
+      <label class="block font-bold"
+        >어떤 말투로 스크립트를 생성하고 싶나요?</label
+      >
+      <div class="grid grid-cols-3 text-center gap-2 gap-y-3 text-lg">
+        <CustomBoxComponent
+          v-for="(item, index) in data"
+          :key="index"
+          :text="item"
+        />
+      </div>
+    </div>
+    <NextButttonComponent :handleClickNextButton="handleClickNextButton" />
+  </div>
+</template>
+
+<script>
+import CustomBoxComponent from "./CustomBoxComponent.vue";
+import NextButttonComponent from "./NextButttonComponent.vue";
+import { useStore } from "@/stores/store";
+import router from "@/router/router";
+
+export default {
+  components: { CustomBoxComponent, NextButttonComponent },
+  setup() {
+    const store = useStore();
+    store.updatePageStep(6);
+    const handleClickNextButton = () => {
+      router.push("/loading");
+    };
+    const data = [
+      "다정한",
+      "공손한",
+      "친근한",
+      "편안한",
+      "감정적",
+      "격양된",
+      "명령적",
+      "강압적",
+      "격식적",
+    ];
+    return {
+      data,
+      handleClickNextButton,
+    };
+  },
+};
+</script>
+
+<style></style>
