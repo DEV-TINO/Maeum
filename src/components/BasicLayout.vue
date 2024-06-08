cd740cd9bbfa32615f4c24c72786d60e4d97ccfe
heedong12 <dlgmldus3289@naver.com>
2024-06-08 19:12:11 +0900

    feat: create BasicLayout

---
+++ src/components/BasicLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-col justify-center items-center h-full">
+    <HeaderBar />
+    <ProgressBar />
+    <div class="flex-1 w-full h-full">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderBar from "./HeaderBar.vue";
+import ProgressBar from "./ProgressBar.vue";
+
+export default {
+  components: {
+    HeaderBar,
+    ProgressBar,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="w-full flex flex-row justify-between items-center px-6">
+    <div class="w-72 h-2 bg-box-color rounded-full relative">
+      <div
+        :style="{ width: `${(store.pageStep / 6) * 100}%` }"
+        class="bg-primary-color rounded-full h-full"
+      ></div>
+    </div>
+    <h1>{{ store.pageStep }}/6</h1>
+  </div>
+</template>
+
+<script setup>
+import { useStore } from "@/stores/store";
+
+const store = useStore();
+</script>
+
+<style scoped></style>
