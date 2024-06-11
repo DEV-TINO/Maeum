e326a92ac05582f1a99728c9cfcef1b50576c5c3
heedong12 <dlgmldus3289@naver.com>
2024-06-11 22:12:49 +0900

    feat: change margin to padding

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -7,7 +7,7 @@ const handleClickMoveToStep1 = () => {
 };
 </script>
 <template>
-  <div class="mt-8 px-2 flex flex-row w-full">
+  <div class="pt-8 px-2 flex flex-row w-full">
     <img
       src="/public/images/header-previous.png"
       @click="handleClickMoveToStep1()"
