255687b046aaf75370f42ff190afc666a4579716
heedong12 <dlgmldus3289@naver.com>
2024-06-17 02:09:13 +0900

    feat: update input component

--- src/components/CustomInputComponent.vue
+++ src/components/CustomInputComponent.vue
@@ -1,3 +1,26 @@
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: "",
+  },
+});
+const emit = defineEmits(["update:modelValue"]);
+
+const inputValue = ref(props.modelValue);
+
+const updateInputValue = (value) => {
+  inputValue.value = value;
+  emit("update:modelValue", value);
+};
+</script>
+
 <template>
-  <input class="bg-box-color outline-none h-12 rounded-xl px-2" />
+  <input
+    class="bg-box-color outline-none h-12 rounded-xl px-2"
+    :value="inputValue"
+    @input="(event) => updateInputValue(event.target.value)"
+  />
 </template>
