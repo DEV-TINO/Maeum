2eeb9eb9c2178e5c255c6372a4509cda02bbb286
heedong12 <dlgmldus3289@naver.com>
2024-06-21 18:27:09 +0900

    feat: change method name

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,11 +1,11 @@
 <script setup>
 import { usePageStepStore } from "@/stores/store";
 
-const { handleClickBackButton } = usePageStepStore();
+const { moveToPreviousPage } = usePageStepStore();
 </script>
 <template>
   <div class="pt-8 px-2 flex flex-row w-full">
-    <img src="/images/header-previous.png" @click="handleClickBackButton" />
+    <img src="/images/header-previous.png" @click="moveToPreviousPage" />
     <div class="flex-1"></div>
   </div>
 </template>
